f4e62b807def2587c16b59cc200334f0ba9cf214
pawelczak <lukaspawelczak@gmail.com>
2022-02-05 09:39:15 +0100

    initial commit

---
+++ apps/movies/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'movies-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'movies';
+}
